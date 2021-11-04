c2855697c7896b10ee6a3c43beca7769f7ea264e
Gustavo Henrique da Rocha Reis <gustavo.reis@ifsudestemg.edu.br>
2021-11-04 00:01:56 -0300

    Alteração nos comentários do código

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ target_host = sys.argv[1]  # Lista de argumentos passadas via linha de comando p
 target_port = 9999  # Porta de conexão com o servidor
 serverAddressPort = (target_host, target_port)  # Atribui a tupla de valores à variável serverAddressPort
 
-cmd = str.encode(sys.argv[2])  # Lista de argumentos passadas via linha de comando para o script Python
+cmd = str.encode(sys.argv[2])  # Lista de argumentos passados via linha de comando para o script Python
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Cria socket utilizando protocolo IPv4 e protocolo UDP
 
@@ -13,4 +13,4 @@ client.sendto(cmd, serverAddressPort)  # Envia conjunto de bytes (mensagens) par
 
 response = client.recvfrom(8192)  # Recebe dados do socket remoto em um determinado tamanho de buffer
 
-print(str(response[0], "utf-8"))  # Converte os byter recebidos em string codificação UTF-8
+print(str(response[0], "utf-8"))  # Converte os bytes recebidos em string codificação UTF-8
